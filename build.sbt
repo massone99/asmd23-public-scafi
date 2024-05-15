2e18d0f627bf9b04bbbc513e9d270bb7eb279e31
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2024-05-15 13:09:51 +0200

    feat: move to scala 3

--- build.sbt
+++ build.sbt
@@ -1,27 +1,19 @@
+import sbt.librarymanagement
 name := "scafi-experiment"
 
 version := "0.1"
 
-scalaVersion := "2.12.4"
+scalaVersion := "3.3.3"
 // build.sbt
-val scafi_version = "v0.3.3"
+val scafi_version = "1.3.0"
 
-val scafi_core  =  "it.unibo.apice.scafiteam" %% "scafi-core" % scafi_version
-val scafi_simulator  =  "it.unibo.apice.scafiteam" %% "scafi-simulator" % scafi_version
-val scafi_simulator_gui =  "it.unibo.apice.scafiteam" %% "scafi-simulator-gui-new" % scafi_version intransitive()
-val scalafx = "org.scalafx" %% "scalafx" % "10.0.2-R15"
+val scafi_core  =  "it.unibo.scafi" %% "scafi-core" % scafi_version
+val scafi_simulator  =  "it.unibo.scafi" %% "scafi-simulator" % scafi_version
+val scafi_simulator_gui =  "it.unibo.scafi" %% "simulator-gui-new" % scafi_version
 
-libraryDependencies ++= Seq(scafi_simulator_gui, scafi_core, scafi_simulator, scalafx)
-fork := true
-lazy val osName = System.getProperty("os.name") match {
-  case n if n.startsWith("Linux") => "linux"
-  case n if n.startsWith("Mac") => "mac"
-  case n if n.startsWith("Windows") => "win"
-  case _ => throw new Exception("Unknown platform!")
-}
-
-lazy val javaFXModules = Seq("base", "controls", "graphics", "media", "swing", "web")
-
-libraryDependencies ++= javaFXModules.map( m=>
-  "org.openjfx" % s"javafx-$m" % "14" classifier osName
+libraryDependencies ++= Seq(
+  scafi_simulator_gui.cross(CrossVersion.for3Use2_13),
+  scafi_core.cross(CrossVersion.for3Use2_13),
+  scafi_simulator.cross(CrossVersion.for3Use2_13)
 )
+fork := true
--- src/main/scala/lab/demo/Programs.scala
+++ src/main/scala/lab/demo/Programs.scala
@@ -2,12 +2,11 @@ package lab.demo
 
 import it.unibo.scafi.incarnations.BasicAbstractIncarnation
 import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.ExportEvaluation.EXPORT_EVALUATION
-import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.SimulationInfo
+import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.{ScafiSimulationInitializer, SimulationInfo}
 import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.configuration.{ScafiProgramBuilder, ScafiWorldInformation}
 import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.world.ScafiWorldInitializer.Random
 import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.ScafiWorldIncarnation.EXPORT
 import it.unibo.scafi.simulation.s2.frontend.view.{ViewSetting, WindowConfiguration}
-import lab.gui.patch.RadiusLikeSimulation
 import it.unibo.scafi.space.graphics2D.BasicShape2D.Circle
 
 import scala.reflect._
@@ -16,7 +15,7 @@ object Incarnation extends BasicAbstractIncarnation
 import lab.demo.Incarnation._ //import all stuff from an incarnation
 
 class Simulation[R: ClassTag] extends App {
-
+  ScafiSimulationInitializer
   val formatter_evaluation: EXPORT_EVALUATION[Any] = (e : EXPORT) => formatter(e.root[Any]())
 
   val formatter: Any => Any = (e) => e match {
@@ -38,7 +37,7 @@ class Simulation[R: ClassTag] extends App {
   ScafiProgramBuilder(
     Random(nodes, width, height),
     SimulationInfo(implicitly[ClassTag[R]].runtimeClass, exportEvaluations = List(formatter_evaluation)),
-    RadiusLikeSimulation(neighbourRange),
+    ScafiSimulationInitializer.RadiusSimulation(neighbourRange),
     ScafiWorldInformation(shape = Some(Circle(5, 5))),
     neighbourRender = true,
   ).launch()
@@ -61,76 +60,78 @@ class Main2 extends AggregateProgramSkeleton {
 }
 object Demo2 extends Simulation[Main2]
 
-class Main3 extends AggregateProgramSkeleton {
+class Main3 extends AggregateProgramSkeleton:
   override def main() = (10,20)
-}
+
 object Demo3 extends Simulation[Main3]
 
-class Main4 extends AggregateProgramSkeleton {
+class Main4 extends AggregateProgramSkeleton:
   override def main() = Math.random()
-}
+
 object Demo4 extends Simulation[Main4]
 
-class Main5 extends AggregateProgramSkeleton {
+class Main5 extends AggregateProgramSkeleton:
   override def main() = sense1
-}
+
 object Demo5 extends Simulation[Main5]
 
-class Main6 extends AggregateProgramSkeleton {
+class Main6 extends AggregateProgramSkeleton:
   override def main() = if (sense1) 10 else 20
-}
+
 object Demo6 extends Simulation[Main6]
 
-class Main7 extends AggregateProgramSkeleton {
+class Main7 extends AggregateProgramSkeleton:
   override def main() = mid()
-}
+
 object Demo7 extends Simulation[Main7]
 
-class Main8 extends AggregateProgramSkeleton {
+class Main8 extends AggregateProgramSkeleton:
   override def main() = minHoodPlus(nbrRange)
-}
+
 object Demo8 extends Simulation[Main8]
 
-class Main9 extends AggregateProgramSkeleton {
+class Main9 extends AggregateProgramSkeleton:
   override def main() = rep(0){_+1}
-}
+
 object Demo9 extends Simulation[Main9]
 
-class Main10 extends AggregateProgramSkeleton {
+class Main10 extends AggregateProgramSkeleton:
   override def main() = rep(Math.random()){x=>x}
-}
+
 object Demo10 extends Simulation[Main10]
 
-class Main11 extends AggregateProgramSkeleton {
+class Main11 extends AggregateProgramSkeleton:
   override def main() = rep[Double](0.0){x => x + rep(Math.random()){y=>y}}
-}
+
 object Demo11 extends Simulation[Main11]
 
-class Main12 extends AggregateProgramSkeleton {
+class Main12 extends AggregateProgramSkeleton:
   import Builtins.Bounded.of_i
 
   override def main() = maxHoodPlus(boolToInt(nbr{sense1}))
-}
+
 object Demo12 extends Simulation[Main12]
 
-class Main13 extends AggregateProgramSkeleton {
+class Main13 extends AggregateProgramSkeleton:
   override def main() = foldhoodPlus(0)(_+_){nbr{1}}
-}
+
 object Demo13 extends Simulation[Main13]
 
-class Main14 extends AggregateProgramSkeleton {
+class Main14 extends AggregateProgramSkeleton:
   import Builtins.Bounded.of_i
 
   override def main() = rep(0){ x => boolToInt(sense1) max maxHoodPlus( nbr{x}) }
-}
+
 object Demo14 extends Simulation[Main14]
 
-class Main15 extends AggregateProgramSkeleton {
-  override def main() = rep(Double.MaxValue){ d => mux[Double](sense1){0.0}{minHoodPlus(nbr{d}+1.0)} }
-}
+class Main15 extends AggregateProgramSkeleton:
+  override def main() = rep(Double.MaxValue):
+    d => mux[Double](sense1){0.0}{minHoodPlus(nbr{d}+1.0)}
+
 object Demo15 extends Simulation[Main15]
 
-class Main16 extends AggregateProgramSkeleton {
-  override def main() = rep(Double.MaxValue){ d => mux[Double](sense1){0.0}{minHoodPlus(nbr{d}+nbrRange)} }
-}
+class Main16 extends AggregateProgramSkeleton:
+  override def main() = rep(Double.MaxValue):
+    d => mux[Double](sense1){0.0}{minHoodPlus(nbr{d}+nbrRange)}
+
 object Demo16 extends Simulation[Main16]
